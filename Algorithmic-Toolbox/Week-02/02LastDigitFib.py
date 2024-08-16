cd757d7d78b3b8dfba82ab35e3403509df39e250
Rayyan Shabbir <rayanshabir1@gmail.com>
2024-08-16 21:22:40 +0500

    Add files via upload

---
+++ Algorithmic-Toolbox/Week-02/02LastDigitFib.py
@@ -0,0 +1,18 @@
+def fib_mod_10(n):
+    # Pisano period for modulo 10 is 60
+    pisano_period = 60
+    n = n % pisano_period
+
+    prv1, prv2 = 0, 1
+
+    if n <= 1:
+        return n
+
+    for i in range(2, n+1):
+        cur = (prv1 + prv2)
+        prv1, prv2 = prv2, cur
+
+    return prv2 % 10
+
+n = int(input())
+print(fib_mod_10(n))
