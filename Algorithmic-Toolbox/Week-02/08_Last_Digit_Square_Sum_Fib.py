63dc78d68ef1f0428a416ccec818eb4c61aa0bdc
Rayyan Shabbir <rayanshabir1@gmail.com>
2024-08-22 06:57:43 +0500

    Update 08_Last_Digit_Square_Sum_Fib.py

--- Algorithmic-Toolbox/Week-02/08_Last_Digit_Square_Sum_Fib.py
+++ Algorithmic-Toolbox/Week-02/08_Last_Digit_Square_Sum_Fib.py
@@ -1,34 +1,26 @@
-# sum of m to n => F(n+2) - F(m+1)
 def fib(n):
+    n = n % 60
+
     if n <= 1:
         return n
-    
-    n = n % 60
 
     prv1 = 0
     prv2 = 1
 
     for i in range(2, n+1):
-        cur = (prv1 + prv2)
+        cur = prv1 + prv2
         prv1 = prv2
         prv2 = cur
 
     return prv2
 
-            
+n = int(input())
 
+fib_n = fib(n)
+fib_n_1 = fib(n+1)
 
-m, n = map(int, input().split(" "))
+# print(fib_n)
+# print(fib_n_1)
 
-if n <= 1:
-    print(n)
-else:
-    fib_1 = fib(m+1)
-    fib_2 = fib(n+2)
-    lst = (fib_2 - fib_1) % 10
-    print(lst)
-
-
-
-
-
+lst = (fib_n * fib_n_1) % 10
+print(lst)
