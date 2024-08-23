61c0b3ce4dd96dc599bc7d14468ef63d63366a3a
Rayyan Shabbir <rayanshabir1@gmail.com>
2024-08-23 12:10:41 -0400

    Add files via upload

---
+++ Algorithmic-Toolbox/Week-05/05_Longest_Common_Subsequence_three_strings.py
@@ -0,0 +1,31 @@
+def lcs_of_three(X, Y, Z):
+    m = len(X)
+    n = len(Y)
+    o = len(Z)
+
+    # Create a 3D array to store lengths of LCS
+    L = [[[0] * (o + 1) for _ in range(n + 1)] for _ in range(m + 1)]
+
+    # Build the L[m+1][n+1][o+1] in bottom-up fashion
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            for k in range(1, o + 1):
+                if X[i - 1] == Y[j - 1] == Z[k - 1]:
+                    L[i][j][k] = L[i - 1][j - 1][k - 1] + 1
+                else:
+                    L[i][j][k] = max(L[i - 1][j][k], L[i][j - 1][k], L[i][j][k - 1])
+
+    # L[m][n][o] contains the length of LCS of X, Y, Z
+    return L[m][n][o]
+
+# Example usage with integer sequences
+n = int(input())  # Length of first sequence
+X = list(map(int, input().split()))  # First sequence
+m = int(input())  # Length of second sequence
+Y = list(map(int, input().split()))  # Second sequence
+l = int(input())  # Length of third sequence
+Z = list(map(int, input().split()))  # Third sequence
+
+# Get the length of the LCS of the three sequences
+lcs_length = lcs_of_three(X, Y, Z)
+print(lcs_length)
